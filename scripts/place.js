305f317a167c5ab41b7a34b63a8feb041883172e
israelessien48-art <israelessien48@gmail.com>
2025-10-30 22:41:36 +0100

    update

---
+++ scripts/place.js
@@ -0,0 +1,20 @@
+// ---------- Footer Script ----------
+document.getElementById("year").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = document.lastModified;
+
+// ---------- Wind Chill Calculation ----------
+const temp = parseFloat(document.getElementById("temp").textContent);
+const speed = parseFloat(document.getElementById("speed").textContent);
+const chill = document.getElementById("chill");
+
+// one-line function as required
+function calculateWindChill(t, s) {
+  return (35.74 + 0.6215*t - 35.75*Math.pow(s,0.16) + 0.4275*t*Math.pow(s,0.16)).toFixed(1);
+}
+
+// call only if conditions are met
+if (temp <= 50 && speed > 3) {
+  chill.textContent = `${calculateWindChill(temp, speed)} °F`;
+} else {
+  chill.textContent = "N/A";
+}
