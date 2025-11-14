11fab640ec0cdba522ada0ae14d235c217d35ec3
israelessien48-art <israelessien48@gmail.com>
2025-11-14 16:42:13 +0100

    update

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -11,19 +11,18 @@ const select = document.querySelector("#product-name");
 if (select) {
     products.forEach(product => {
         const opt = document.createElement("option");
-        opt.value = product.id;
-        opt.textContent = product.name;
+        opt.value = product.id;       // ID as value
+        opt.textContent = product.name; // Name as display
         select.appendChild(opt);
     });
 }
 
-// Track last modified
+// Track review count and last modified
 const lastModElem = document.getElementById("lastModified");
 if(lastModElem) {
     lastModElem.textContent = document.lastModified;
 }
 
-// Track review count
 const countElem = document.getElementById("count");
 if(countElem) {
     let count = Number(localStorage.getItem("reviewCount")) || 0;
